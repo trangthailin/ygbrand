99d73da7eadf79fd1567e0606bf8632f6b3c31da
trangthailin <157803368+trangthailin@users.noreply.github.com>
2024-01-26 19:01:31 +0700

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:4000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+})
+
+$('#slider').owlCarousel({
+loop:true,	// cho lap lại
+	items:1,	// xuất hiện 1 ảnh thôi
+	smartSpeed:1000,   	// tốc độ thay đổi ảnh
+	autoplay:true,		// cho phép tự động chạy
+    autoplayTimeout:4000,  // thời gian chờ khi chuyển ảnh khi chạy tự động
+	nav:true,	// cho xuất hiện bộ nút tới lui 
+})
+
+
+
